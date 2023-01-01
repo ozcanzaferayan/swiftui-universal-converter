80a4254b3ac7ff08a0a5ccb83604b3f61ad660d4
Zafer AYAN <ozcanzaferayan@gmail.com>
2023-01-01 17:17:04 +0300

    Initial Commit

---
+++ UniversalConverter/UniversalConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversalConverterApp.swift
+//  UniversalConverter
+//
+//  Created by Zafer AYAN on 1.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct UniversalConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
